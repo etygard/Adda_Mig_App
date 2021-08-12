98935f87db3b4b1ea71874bcf7a3db06389b75bb
AddaMigApp <emma.tygard@gmail.com>
2021-08-12 19:06:12 +0200

    Initial Commit

---
+++ Adda_Mig_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Adda_Mig_App
+//
+//  Created by Emma tygard on 2021-08-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
